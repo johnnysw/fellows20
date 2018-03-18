9ac6c128f345606b6d2a4409d8c2d8d1e9760dec
fengchuanyu <fengchuanyu@163.com>
2018-03-18 09:38:06 +0800

    new file

--- requirejs/弹出框/js/index.js
+++ requirejs/弹出框/js/index.js
@@ -1,6 +1,31 @@
-require(["dialog"],function (dialog) {
-    var oBtn = document.getElementById("btn");
-    oBtn.onclick = function () {
-        dialog.open();
+requirejs.config({
+    paths: {
+        jquery: 'jquery-3.3.1'
     }
+});
+require(["dialog","jquery"],function (Dialog,$) {
+    // var oBtn = $("#btn");
+    // oBtn.onclick = function () {
+    //     dialog.open();
+    // }
+    var settings = {
+        height:500,
+        title:"登录",
+        content:"a.html"
+    };
+    var settings1 = {
+        height:300,
+        title:"登录",
+        content:"登录成功"
+    }
+    $("#btn1").click(function () {
+        var dialog1= new Dialog();
+        dialog1.open(settings);
+    });
+    $("#btn").click(function () {
+        var dialog= new Dialog();
+        dialog.open(settings1);
+    })
+    //AMD
+    //CMD
 })
